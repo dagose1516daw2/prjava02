adaa4c9b63381e0e85e865f82292db98a5d77bc6
dagose1516daw2 <dagose1516daw2@gmail.com>
2015-11-07 17:56:27 +0100

    Treballant amb branques ordres Git i branques v=.1

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -29,7 +29,8 @@ package prjava02;
             		System.out.println("Nom de l'usuari: " + System.getProperty("user.name")); 
             		System.out.println("Carpeta Personal: " + System.getProperty("user.home")); 
             		System.out.println("Sistema operatiu: " + System.getProperty("os.name")); 
-            		System.out.println("Versió OS: " + System.getProperty("os.version")); 
+            		System.out.println("Versió OS: " + System.getProperty("os.version"));
+                        System.out.println("Creació d'una branca del projecte prjava02");
         	       } 
         	       catch (IOException e) { 
         	       }  
