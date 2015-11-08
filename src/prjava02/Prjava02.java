0cfde940c5d14fb67c80095ce676425dc383c4ea
dagose1516daw2 <dagose1516daw2@gmail.com>
2015-11-08 12:33:48 +0100

    Creant primer conflicte amb la branca01

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -5,11 +5,7 @@
  */
 package prjava02;
 
-/* 
-	 * To change this template, choose Tools | Templates 
-	 * and open the template in the editor. 
-	 */ 
-	
+
 	import java.io.*; 
 	import java.net.*; 
 	/** 
@@ -17,23 +13,21 @@ package prjava02;
 	 * @author Daniel Gonzalez Segarra
 	 */ 
 	public class Prjava02 { 
-	    /** 
-	     * @param args the command line arguments 
-	     */ 
+	    
 	    public static void main(String[] args) { 
 	        System.out.println("versió 0.1 del projecte prjava02"); 
 	        try { 
             		InetAddress adreça = InetAddress.getLocalHost(); 
             		String hostname = adreça.getHostName(); 
             		System.out.println("hostname="+hostname); 
-			System.out.println("Conflicte típic: Codi diferent a una mateixa línia"); 
             		System.out.println("Nom de l'usuari: " + System.getProperty("user.name")); 
             		System.out.println("Carpeta Personal: " + System.getProperty("user.home")); 
             		System.out.println("Sistema operatiu: " + System.getProperty("os.name")); 
             		System.out.println("Versió OS: " + System.getProperty("os.version"));
-                        System.out.println("Creació d'una branca del projecte prjava02");
-			System.out.println("Afegint més codi a la branca00 del projecte prjava02");
-			System.out.println("Afegint codi a la branca01 del projecte prjava02");
+                    System.out.println("Creació d'una branca del projecte prjava02");
+					System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+					System.out.println("Afegint codi a la branca01 del projecte prjava02");
+					System.out.println("Conflicte típic: Codi diferent a una mateixa línia"); 
         	       } 
         	       catch (IOException e) { 
         	       }  
