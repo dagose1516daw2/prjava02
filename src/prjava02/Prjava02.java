f32d2ecff0e6d01715d3f776791c83a3506e8391
dagose1516daw2 <dagose1516daw2@gmail.com>
2015-11-08 12:36:41 +0100

    branca master

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -4,22 +4,18 @@
  * and open the template in the editor.
  */
 package prjava02;
-
-/* 
-	 * To change this template, choose Tools | Templates 
+	 /*
+	  *  To change this template, choose Tools | Templates 
 	 * and open the template in the editor. 
 	 */ 
 	
 	import java.io.*; 
 	import java.net.*; 
-	/** 
-	 * 
+	/**
 	 * @author Daniel Gonzalez Segarra
 	 */ 
 	public class Prjava02 { 
-	    /** 
-	     * @param args the command line arguments 
-	     */ 
+	
 	    public static void main(String[] args) { 
 	        System.out.println("versió 0.1 del projecte prjava02"); 
 	        try { 
@@ -30,8 +26,8 @@ package prjava02;
             		System.out.println("Carpeta Personal: " + System.getProperty("user.home")); 
             		System.out.println("Sistema operatiu: " + System.getProperty("os.name")); 
             		System.out.println("Versió OS: " + System.getProperty("os.version"));
-                        System.out.println("Creació d'una branca del projecte prjava02");
-			System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+                    System.out.println("Creació d'una branca del projecte prjava02");
+					System.out.println("Afegint més codi a la branca00 del projecte prjava02");
         	       } 
         	       catch (IOException e) { 
         	       }  
